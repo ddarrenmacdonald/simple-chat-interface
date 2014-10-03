7e5df32615b7bb7e8e30940cc0f0aff111ff48c1
D. Darren MacDonald <drrnmcdnld@gmail.com>
2014-10-03 11:40:17 -0300

    Changes....again!
    I am trying to clean up my code here!

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function sendMessage(event){
     var msg = document.getElementById("usrMsg").value;
-    var text = "";
     var para = document.createElement("P");
 	var txtnode = document.createTextNode(msg);
 	para.appendChild(txtnode);
