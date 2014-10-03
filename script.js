0d03f0cb67c72df374bdd93b3bba74285e9770bf
D. Darren MacDonald <drrnmcdnld@gmail.com>
2014-10-03 11:32:08 -0300

    Functional?
    I think I now have functionality!

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-var name
-name = prompt("What is your name?");
-
+function sendMessage(event){
+    var msg = document.getElementById("usrMsg").value;
+    var text = "";
+    var para = document.createElement("P");
+	var txtnode = document.createTextNode(msg);
+	para.appendChild(txtnode);
+	document.getElementById("chatBox").appendChild(para);
+	document.getElementById("usrMsg").value="";
+};
